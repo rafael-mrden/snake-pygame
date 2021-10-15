83ed3a6b1e18b6c5ea5ed7f5202fa7bb45cd86e0
rafael-mrden <rafaelmrdjen@gmail.com>
2021-10-15 02:02:53 +0200

    Added graph of the complement

--- snake/utils.py
+++ snake/utils.py
@@ -1,5 +1,6 @@
 from random import randint, shuffle
 from pygame.math import Vector2
+import networkx as nx
 
 def print_text(message, position, color, font):
     '''Puts text on surface and returns it.'''
@@ -48,22 +49,52 @@ def make_decision(head, body, direction, food, sw, sh):
     directions = [v for v in [Vector2(v_) for v_ in [(1, 0), (-1, 0), (0, 1), (0, -1)]] \
                         if (v != -direction) and (modulo(head + v, sw, sh) not in body)]
     
-    if directions == []: # Game over in the next step
+    if directions == []: # Too bad. Game over in the next step
         return direction
     
     if len(directions) == 1: # No choice
         return directions[0]
     
     current_distance = torus_distance(head, food, sw, sh)
+    complement_graph = make_graph(body, sw, sh)
     
-    # shuffle(directions) To many edges with this option.
-    for v in directions:
-        if torus_distance(head + v, food, sw, sh) < current_distance:
+    directions_open_path = {} # Here we will record whether it is possible
+                              # to reach food by moving in the directions.
+    
+    for i in range(len(directions)):
+        v = directions[i]
+        new_head = Vector2(modulo(head + v, sw, sh))
+        
+        # Do we get closer to food?
+        shorter_path = (torus_distance(new_head, food, sw, sh) < current_distance)
+        
+        # Is it even possible to reach food this way?
+        open_path = nx.has_path(complement_graph, modulo(new_head, sw, sh), modulo(food, sw, sh))
+        if open_path and shorter_path:
             return v
+        
+        directions_open_path[i] = open_path
     
-    # TODO: Make better decisions by observing the connected components
-    # of the graph obtained from the complement of the snake's body.
+    restricted_possibilities = [v for v in directions if directions_open_path[directions.index(v)] == True]
     
-    return directions[randint(0, len(directions) - 1)]
+    if restricted_possibilities != []:
+        return restricted_possibilities[0]
+    
+    else:
+        return directions[randint(0, len(directions) - 1)]
+
+
+def make_graph(body, sw, sh):
+    '''Makes graph from the complement of the body of the snake.'''
+    
+    vertices = [(i,j) for i in range(sw) for j in range(sh) if (i,j) not in body]
+    
+    edges = { v : [ modulo(Vector2(v) + Vector2(d), sw, sh) for d in [(1, 0), (-1, 0), (0, 1), (0, -1)] \
+                if modulo(Vector2(v) + Vector2(d), sw, sh) not in body] \
+                    for v in vertices }
+    
+    G = nx.Graph(edges)
+    return G
+    
 
     
